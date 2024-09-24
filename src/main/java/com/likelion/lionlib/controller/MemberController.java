144c70a59e1adee815c09e05c45db5334ec9ec4f
정재웅 <jweidon0821@gmail.com>
2024-09-24 17:45:34 +0900

    Feat: 회원 관련 API 구현

--- src/main/java/com/likelion/lionlib/controller/MemberController.java
+++ src/main/java/com/likelion/lionlib/controller/MemberController.java
@@ -1,15 +1,14 @@
 package com.likelion.lionlib.controller;
 
 import com.likelion.lionlib.dto.LoginRequest;
+import com.likelion.lionlib.dto.MemberResponse;
+import com.likelion.lionlib.dto.ProfileRequest;
 import com.likelion.lionlib.dto.SignupRequest;
 import com.likelion.lionlib.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -34,5 +33,23 @@ public class MemberController {
         } else {
             return ResponseEntity.status(401).body("로그인 실패: 잘못된 이메일 또는 비밀번호");
         }
+    }
+
+    // 회원 정보 조회
+    @GetMapping("/members/{memberId}")
+    public ResponseEntity<MemberResponse> getMember(@PathVariable Long memberId) {
+        log.info("Request GET member with ID: {}", memberId);
+        MemberResponse memberResponse = memberService.findMember(memberId);
+        return ResponseEntity.ok(memberResponse);
+    }
+
+    // 프로필 수정
+    @PutMapping("/members/{memberId}")
+    public ResponseEntity<MemberResponse> putMember(
+            @PathVariable Long memberId,
+            @RequestBody ProfileRequest updateRequest) {
+        log.info("Request PUT update member with ID: {}", memberId);
+        MemberResponse updatedMember = memberService.updateMember(memberId, updateRequest);
+        return ResponseEntity.ok(updatedMember);
     }
 }
--- src/main/java/com/likelion/lionlib/dto/LoginRequest.java
+++ src/main/java/com/likelion/lionlib/dto/LoginRequest.java
@@ -2,10 +2,8 @@ package com.likelion.lionlib.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class LoginRequest {
     private String email;
